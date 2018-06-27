81f8b9aa82024033bb403497fb530710c2961a4e
anthonyrabiaza <anthony.rabiaza@gmail.com>
2018-06-27 11:44:36 +0800

    Adding SSL support
    HTTPdLight can use a private key (JKS) to expose SSL connection.  A self-signed jks file is provided. Also pem (private) and der (public) certificates are available.

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ HTTPdLight is a lightweight HTTP Server used to Mock endpoints. It requires only
 
 ## Getting Started
 
-Please download the [jar](jar/httpdlight-0.1.jar) from github 
+Please download the [jar](jar/httpdlight-0.2.jar?raw=true) from github.
+For Testing with SSL, you can also download the self-signed certificate [jks](httpdlight.jks?raw=true). Apart from that, you have:
+
+- the private certificate is available here [pem](httpdlight.pem?raw=true)
+- the public certificate is available here [der](httpdlight.der?raw=true)
 
 ### Java Check
 
@@ -23,7 +27,7 @@ Java HotSpot(TM) 64-Bit Server VM (build 20.45-b01, mixed mode)
 The default port is 8008, the default uri is /rnif/partner1 and the default reply is an empty String ""
 
 ```
-ant@myserver:~$ java -jar httpdlight-0.1.jar
+ant@myserver:~$ java -jar httpdlight-0.2.jar
 Starting HTTPServer at port 8008, URI /rnif/partner1
 Started!
 
@@ -31,7 +35,8 @@ Started!
 
 #### Testing
 
-Setup Postman to call the HTTPdLight server, in the following exemple we are running Postman in the same server.
+
+Setup Postman to call the HTTPdLight server, in the following example we are running Postman in the same server.
 If we are running Postman in the different server replace localhost with the IP or the name of the server where HTTPdLight is running.
 
 ![Alt text](resources/Postman_Default.png?raw=true "HTTPdLight")
@@ -62,7 +67,7 @@ Hello World
 You can override the port, the URI or the reply. On the following example, we are overriden all of the elements:
 
 ```
-ant@myserver:~$ java -DPORT=8009 -DURI="/rnif/partner3" -DREPLY="<ok/>" -jar httpdlight-0.1.jar
+ant@myserver:~$ java -DPORT=8009 -DURI="/rnif/partner3" -DREPLY="<ok/>" -jar httpdlight-0.2.jar
 Found overridden uri: /rnif/partner3
 Found overridden port: 8009
 Found overridden reply: <ok/>
@@ -72,7 +77,7 @@ Started!
 
 ### Testing
 
-Setup Postman to call the HTTPdLight server, in the following exemple we are running Postman in the same server.
+Setup Postman to call the HTTPdLight server, in the following example we are running Postman in the same server.
 If we are running Postman in the different server replace localhost with the IP or the name of the server where HTTPdLight is running.
 
 ![Alt text](resources/Postman_Override.png?raw=true "HTTPdLight")
@@ -95,4 +100,44 @@ Body :
 Hello World
 </message>
 ########## Wed Jun 13 12:47:58 SGT 2018 End of Receiving request ##########
+```
+
+### Run the Server with SSL
+
+You can override the port, the URI or the reply. On the following example, we are overriden all of the elements:
+
+```
+ant@myserver:~$ java -DSSLCERT=httpdlight.jks -DSSLPASSWORD=password -DREPLY="<ok/>" -jar httpdlight-0.2.jar
+Found overridden reply: <ok/>
+Found overridden SSLCERT: httpdlight.jks
+Found overridden SSLPASSWORD: password
+Starting HTTPServer at port 8009, URI /rnif/partner1 (with SSL)
+Started!
+```
+
+### Testing
+
+Setup Postman to call the HTTPdLight server, disable SSL certification verification (Settings>General>Request>SSL certification verification), in the following example we are running Postman in the same server.
+If we are running Postman in the different server replace localhost with the IP or the name of the server where HTTPdLight is running.
+
+![Alt text](resources/Postman_SSL.png?raw=true "HTTPdLight")
+
+```
+########## Wed Jun 27 11:28:00 SGT 2018 Receiving request ##########
+Receiving request /rnif/partner1
+Headers
+        Accept-encoding:[gzip, deflate]
+        Accept:[*/*]
+        Connection:[keep-alive]
+        Host:[localhost:8008]
+        User-agent:[PostmanRuntime/7.1.5]
+        Content-type:[application/xml]
+        Postman-token:[24ae7914-280f-4c81-8c0c-a3d775c08c5d]
+        Content-length:[32]
+        Cache-control:[no-cache]
+Body :
+<message>
+Hello World
+</message>
+########## Wed Jun 27 11:28:00 SGT 2018 End of Receiving request ##########
 ```
--- src/com/boomi/proserv/httpd/server/HTTPServer.java
+++ src/com/boomi/proserv/httpd/server/HTTPServer.java
@@ -1,42 +1,55 @@
 package com.boomi.proserv.httpd.server;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.net.InetSocketAddress;
+import java.security.KeyStore;
 import java.util.Date;
 import java.util.Iterator;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsServer;
 
 /**
  * HTTPServer Class: this is a lightweight HTTP server used to Mock endpoints which require only a JRE/JDK installed (JDK 6+)
  * Default port is 8008 and default uri is /rnif/partner1 <br/>
- * To override these values use -D with PORT or URI as follow:
+ * To override these values use -D with PORT, URI or REPLY as follow:
  * java -DPORT=8009 -DURI="/rnif/partner3" -DREPLY="<ok/>" -jar httpdlight-0.1.jar
  * @author anthony.rabiaza@gmail.com
  *
  */
 public class HTTPServer {
 	
-	private static String URI 	= "/rnif/partner1"; // Should start with "/" 
-	private static Integer PORT = 8008;
-	private static String REPLY = "";
+	private static String URI 			= "/rnif/partner1"; // Should start with "/" 
+	private static Integer PORT 		= 8008;
+	private static String REPLY 		= "";
+	private static String SSLCERT 		= "";
+	private static String SSLPASSWORD 	= "";
 
     public static void main(String[] args) throws Exception {
     	if(args!=null && args.length>0) {
     		System.out.println("HTTPServer Class, this is a lightweight HTTP server which require only a JRE/JDK installed (JDK 6+).");
     		System.out.println("Default port is 8008 and default uri is /rnif/partner1");
 			System.out.println("To override these values use -D with PORT or URI as follow:");
-			System.out.println("java -DPORT=8001 -DURI=\"/root\" -DREPLY=\"/<ok/>\" -jar httpdlight-0.1.jar");
+			System.out.println("java -DPORT=8009 -DURI=\"/rnif/partner3\" -DREPLY=\"/<ok/>\" -DSSLCERT=httpdlight.jks -DSSLPASSWORD=password -jar httpdlight-0.1.jar");
     	} else {
-	    	String uri = URI;
-	    	int port = PORT;
-	    	String reply = REPLY;
+    		HttpServer server;
+	    	String uri 			= URI;
+	    	int port 			= PORT;
+	    	String reply 		= REPLY;
+	    	String sslCert 		= SSLCERT;
+	    	String sslPassword 	= SSLPASSWORD;
+	    	
 	    	if(System.getProperty("URI")!=null && !System.getProperty("URI").equals("")) {
 	    		uri = System.getProperty("URI");
 	    		System.out.println("Found overridden uri: " + uri);
@@ -49,13 +62,39 @@ public class HTTPServer {
 	    		reply = System.getProperty("REPLY");
 	    		System.out.println("Found overridden reply: " + reply);
 	    	}
-	    	System.out.println("Starting HTTPServer at port "+ port + ", URI " + uri);
-	        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+	    	if(System.getProperty("SSLCERT")!=null && !System.getProperty("SSLCERT").equals("")) {
+	    		sslCert = System.getProperty("SSLCERT");
+	    		System.out.println("Found overridden SSLCERT: " + sslCert);
+	    		if(System.getProperty("SSLPASSWORD")!=null && !System.getProperty("SSLPASSWORD").equals("")) {
+	    			sslPassword = System.getProperty("SSLPASSWORD");
+	    			System.out.println("Found overridden SSLPASSWORD: " + sslPassword);
+	    		}
+	    	}
+	    	System.out.println("Starting HTTPServer at port "+ port + ", URI " + uri + (!sslCert.equals("")?" (with SSL)":""));
+	        
+	        if(sslCert.equals("")) {
+	        	server = HttpServer.create(new InetSocketAddress(port), 0);
+    		} else {
+    			server = HttpsServer.create(new InetSocketAddress(port), 0);
+    			((HttpsServer)server).setHttpsConfigurator(getSSLconfigurator(sslCert, sslPassword));
+    		}
 	        server.createContext(uri, new StandardHandler(reply));
 	        server.setExecutor(null); // creates a default executor
 	        server.start();
 	        System.out.println("Started!");
     	}
+    }
+    
+    static private HttpsConfigurator getSSLconfigurator(String jks, String password) throws Exception {
+    	SSLContext sslContext = SSLContext.getInstance("TLS");
+        char[] keystorePassword = password.toCharArray();
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream(jks), keystorePassword);
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, keystorePassword);
+        sslContext.init(kmf.getKeyManagers(), null, null);
+        HttpsConfigurator configurator = new HttpsConfigurator(sslContext);
+        return configurator;
     }
 
     static class StandardHandler implements HttpHandler {
